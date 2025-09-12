b1598620fa40d844a0a8418290d2d79a1cca2bb2
0xMordecai <sir.lahrach.mohamed@gmail.com>
2025-09-12 21:22:06 +0100

    add

--- chapter3/Creating-Customized-Errors/main.go
+++ chapter3/Creating-Customized-Errors/main.go
@@ -1,17 +1,20 @@
 package main
 
-func main() {}
+func main() {
 
-// -->> Creating Customized Errors
-/*
-	The errors.New function is very straightforward.
+}
+func Demo() {
+	// -->> Creating Customized Errors
+	/*
+		The errors.New function is very straightforward.
 
-	-->			err := errors.New("Syntax error in the code")
-*/
+		-->			err := errors.New("Syntax error in the code")
+	*/
 
-/*
-	The fmt.Errorf function, like many of the functions in the fmt
-	function, allows for formatting within the string.
+	/*
+	   The fmt.Errorf function, like many of the functions in the fmt
+	   function, allows for formatting within the string.
 
-	-->			err := fmt.Errorf("Syntax error in the code at line %d", line)
-*/
+	   -->			err := fmt.Errorf("Syntax error in the code at line %d", line)
+	*/
+}
