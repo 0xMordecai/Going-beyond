d201c201d40fac90a9932a3fbd76ce60166f9433
0xMordecai <sir.lahrach.mohamed@gmail.com>
2025-09-12 22:12:49 +0100

    add

--- chapter3/Wrapping Error with Other Errors/main.go
+++ chapter3/Wrapping Error with Other Errors/main.go
@@ -7,8 +7,13 @@ import (
 
 func main() {}
 
-func ErrWrap() any {
+func ErrWrap() error {
 	err1 := errors.New("Oops Something happend.")
 	err2 := fmt.Errorf("An error was encountered - %w", err1)
 	return err2
+}
+func ErrUnwrap() error {
+	err2 := ErrUnwrap()
+	err1 := errors.Unwrap(err2)
+	return err1
 }
