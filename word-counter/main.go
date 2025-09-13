6d58b276ff2640fef240db69c7fc1486c2533429
0xMordecai <sir.lahrach.mohamed@gmail.com>
2025-09-13 22:37:48 +0100

    add

--- word-counter/main.go
+++ word-counter/main.go
@@ -7,6 +7,8 @@ import (
 )
 
 func main() {
+	// Calling the count function to count the number of words
+	// received from the Standard Input and printing it out
 	fmt.Println(count(os.Stdin))
 }
 
