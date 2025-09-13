4aa4cc64a219f26e2cf8d4acd9139dcdd5e148dc
0xMordecai <sir.lahrach.mohamed@gmail.com>
2025-09-14 00:23:47 +0100

    add

--- word-counter/main_test.go
+++ word-counter/main_test.go
@@ -19,3 +19,12 @@ func TestCountWords(t *testing.T) {
 		t.Errorf("Expected %d, got %d instead. \n", exp, res)
 	}
 }
+
+func TestCountLines(t *testing.T) {
+	b := bytes.NewBufferString("word1 word2 word3\nline2\nline3 word1")
+	exp := 3
+	res := count(b, true)
+	if res != exp {
+		t.Errorf("Expected %d, got %d instead.\n", exp, res)
+	}
+}
