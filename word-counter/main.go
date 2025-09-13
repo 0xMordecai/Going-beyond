dcefb88795ff11a4f7670467ace0cb576c7eb91d
0xMordecai <sir.lahrach.mohamed@gmail.com>
2025-09-13 23:45:04 +0100

    add

--- word-counter/main.go
+++ word-counter/main.go
@@ -2,15 +2,22 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"io"
 	"os"
 )
 
 func main() {
+	// Defining a boolean flag -l to count lines instead of words
+	lines := flag.Bool("l", false, "Count lines")
+
+	// Parsing the flags provided by the user
+	flag.Parse()
+
 	// Calling the count function to count the number of words
 	// received from the Standard Input and printing it out
-	fmt.Println(count(os.Stdin))
+	fmt.Println(count(os.Stdin, *lines))
 }
 
 func count(r io.Reader) int {
