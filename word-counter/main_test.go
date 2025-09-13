05bbdac38149c38667316d87e5c521d9a3a14c48
0xMordecai <sir.lahrach.mohamed@gmail.com>
2025-09-13 23:25:53 +0100

    add

--- word-counter/main_test.go
+++ word-counter/main_test.go
@@ -1,1 +1,11 @@
 package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+// TestCountWords test the count function set to count words
+func TestCountWords(t *testing.T) {
+	b := bytes.NewBufferString("word1 word2 word3 word4\n")
+}
