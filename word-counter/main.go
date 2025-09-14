bf8009375f124a178f18b492f81aa4a8d32d05d9
0xMordecai <sir.lahrach.mohamed@gmail.com>
2025-09-14 22:13:43 +0100

    add

--- word-counter/main.go
+++ word-counter/main.go
@@ -12,15 +12,18 @@ func main() {
 	// Defining a boolean flag -l to count lines instead of words
 	lines := flag.Bool("l", false, "Count lines")
 
+	// Defining a boolean flag -b to count the number of bytes in addition to words and line
+	bytes := flag.Bool("b", false, "Count bytes")
+
 	// Parsing the flags provided by the user
 	flag.Parse()
 
 	// Calling the count function to count the number of words
 	// received from the Standard Input and printing it out
-	fmt.Println(count(os.Stdin, *lines))
+	fmt.Println(count(os.Stdin, *lines, *bytes))
 }
 
-func count(r io.Reader, countLines bool) int {
+func count(r io.Reader, countLines bool, countBytes bool) int {
 	// A scanner is used to read text from a Reader (such as files)
 	scanner := bufio.NewScanner(r)
 	// If the count lines flag is not set, we want to count words so we define
